33d894334c2db255faf89275785f4443177a10ea
Robert Chou <rbtchc+github@gmail.com>
2015-11-23 14:03:31 +0800

    Inherit GenericBank

--- stdchartered.py
+++ stdchartered.py
@@ -7,31 +7,41 @@ from bs4 import BeautifulSoup
 from helper import fetch_url
 from helper import GenericBank
 
-currency_table = {
-    u'\u6fb3\u5e63\uff08AUD\uff09':             'AUD',
-    u'\u52a0\u62ff\u5927\u5e63\uff08CAD\uff09': 'CAD',
-    u'\u745e\u58eb\u6cd5\u90ce\uff08CHF\uff09': 'CHF',
-    u'\u4eba\u6c11\u5e63\uff08CNY\uff09':       'CNY',
-    u'\u6b50\u5143\uff08EUR\uff09':             'EUR',
-    u'\u82f1\u938a\uff08GBP\uff09':             'GBP',
-    u'\u65e5\u5713\uff08JPY\uff09':             'JPY',
-    u'\u7d10\u5e63\uff08NZD\uff09':             'NZD',
-    u'\u745e\u5178\u514b\u6717\u5e63 (SEK)':    'SEK',
-    u'\u65b0\u52a0\u5761\uff08SGD\uff09':       'SGD',
-    u'\u6cf0\u5e63\uff08THB\uff09':             'THB',
-    u'\u6e2f\u5e63\uff08HKD\uff09':             'HKD',
-    u'\u7f8e\u5143\uff08USD\uff09':             'USD',
-    u'\u5357\u975e\u5e63\uff08ZAR\uff09':       'ZAR',
-}
+class STDCharteredBank(GenericBank):
 
-data = {}
-content = fetch_url("http://www.standardchartered.com.tw/check/inquiry-rate-foreign-exchange.asp")
-soup = BeautifulSoup(content, "lxml")
-for tr in soup.table.find_all(['tr'])[1:]:
-    key = currency_table.get(tr.th.getText(), None)
-    value = [None if x.getText() == '-' else float(x.getText()) for x in tr.find_all(['td'])]
-    data[key] = value
-data = {'data': data}
-data['date'] = GenericBank._get_epoch()
+    def __init__(self):
+        super(STDCharteredBank, self).__init__()
+        self.currency_table = {
+            u'\u6fb3\u5e63\uff08AUD\uff09':             'AUD',
+            u'\u52a0\u62ff\u5927\u5e63\uff08CAD\uff09': 'CAD',
+            u'\u745e\u58eb\u6cd5\u90ce\uff08CHF\uff09': 'CHF',
+            u'\u4eba\u6c11\u5e63\uff08CNY\uff09':       'CNY',
+            u'\u6b50\u5143\uff08EUR\uff09':             'EUR',
+            u'\u82f1\u938a\uff08GBP\uff09':             'GBP',
+            u'\u65e5\u5713\uff08JPY\uff09':             'JPY',
+            u'\u7d10\u5e63\uff08NZD\uff09':             'NZD',
+            u'\u745e\u5178\u514b\u6717\u5e63 (SEK)':    'SEK',
+            u'\u65b0\u52a0\u5761\uff08SGD\uff09':       'SGD',
+            u'\u6cf0\u5e63\uff08THB\uff09':             'THB',
+            u'\u6e2f\u5e63\uff08HKD\uff09':             'HKD',
+            u'\u7f8e\u5143\uff08USD\uff09':             'USD',
+            u'\u5357\u975e\u5e63\uff08ZAR\uff09':       'ZAR',
+        }
 
-GenericBank.dump_data(data)
+    def query_rate(self):
+        data = {}
+        content = fetch_url("http://www.standardchartered.com.tw/check/inquiry-rate-foreign-exchange.asp")
+        soup = BeautifulSoup(content, "lxml")
+        for tr in soup.table.find_all(['tr'])[1:]:
+            key = self.currency_table.get(tr.th.getText(), None)
+            value = [None if x.getText() == '-' else float(x.getText()) for x in tr.find_all(['td'])]
+            data[key] = value
+        data = {'data': data}
+        data['date'] = GenericBank._get_epoch()
+        return data
+
+if __name__ == '__main__':
+    bank = STDCharteredBank()
+    data = bank.query_rate()
+    bank.dump_data(data)
+
--- twbank.py
+++ twbank.py
@@ -10,45 +10,55 @@ from bs4 import BeautifulSoup
 from helper import fetch_url
 from helper import GenericBank
 
-currency_table = {
-    u'\xa0Australian Dollar (AUD)':  'AUD',
-    u'\xa0Canadian Dollar (CAD)':    'CAD',
-    u'\xa0Swiss Franc (CHF)':        'CHF',
-    u'\xa0China Yen (CNY)':          'CNY',
-    u'\xa0Euro (EUR)':               'EUR',
-    u'\xa0British Pound (GBP)':      'GBP',
-    u'\xa0Hong Kong Dollar (HKD)':   'HKD',
-    u'\xa0Indonesian Rupiah (IDR)':  'IDR',
-    u'\xa0Japanese Yen (JPY)':       'JPY',
-    u'\xa0Korean Won (KRW)':         'KRW',
-    u'\xa0Malaysian Ringgit (MYR)':  'MYR',
-    u'\xa0New Zealand Dollar (NZD)': 'NZD',
-    u'\xa0Philippine Peso (PHP)':    'PHP',
-    u'\xa0Swedish Krona (SEK)':      'SEK',
-    u'\xa0Singapore Dollar (SGD)':   'SGD',
-    u'\xa0Thai Baht (THB)':          'THB',
-    u'\xa0American Dollar (USD)':    'USD',
-    u'\xa0Vietnam Dong (VND)':       'VND',
-    u'\xa0South African Rand (ZAR)': 'ZAR',
-}
+class TWBank(GenericBank):
 
-content = fetch_url("http://rate.bot.com.tw/Pages/Static/UIP003.en-US.htm")
-soup = BeautifulSoup(content, "lxml")
-tables = soup.find_all('table')
+    def __init__(self):
+        super(TWBank, self).__init__()
 
-quote_date_str = re.findall(ur'Quoted Date[\uff1a][\xa0](.*)\r', tables[4].find_all('td')[:][-1].text)
-if not quote_date_str:
-    logging.error("Unable to parse quoted date from html!")
-# Hardcoded as GMT+8 (CST)
-quote_date = calendar.timegm(time.strptime(quote_date_str[0]+' CST', "%m/%d/%Y %H:%M %Z"))
+        self.currency_table = {
+            u'\xa0Australian Dollar (AUD)':  'AUD',
+            u'\xa0Canadian Dollar (CAD)':    'CAD',
+            u'\xa0Swiss Franc (CHF)':        'CHF',
+            u'\xa0China Yen (CNY)':          'CNY',
+            u'\xa0Euro (EUR)':               'EUR',
+            u'\xa0British Pound (GBP)':      'GBP',
+            u'\xa0Hong Kong Dollar (HKD)':   'HKD',
+            u'\xa0Indonesian Rupiah (IDR)':  'IDR',
+            u'\xa0Japanese Yen (JPY)':       'JPY',
+            u'\xa0Korean Won (KRW)':         'KRW',
+            u'\xa0Malaysian Ringgit (MYR)':  'MYR',
+            u'\xa0New Zealand Dollar (NZD)': 'NZD',
+            u'\xa0Philippine Peso (PHP)':    'PHP',
+            u'\xa0Swedish Krona (SEK)':      'SEK',
+            u'\xa0Singapore Dollar (SGD)':   'SGD',
+            u'\xa0Thai Baht (THB)':          'THB',
+            u'\xa0American Dollar (USD)':    'USD',
+            u'\xa0Vietnam Dong (VND)':       'VND',
+            u'\xa0South African Rand (ZAR)': 'ZAR',
+        }
 
-data = {}
-# extract currency exchange data from sixth table
-for tr in tables[6].find_all('tr'):
-    tds = [td.text for td in tr.find_all('td')]
-    if not tds:
-        continue
-    data[currency_table[tds[0]]] = [float(x) if x != '-' else None for x in tds[1:5]]
-data = {"date": quote_date, "data": data}
+    def query_rate(self):
+        content = fetch_url("http://rate.bot.com.tw/Pages/Static/UIP003.en-US.htm")
+        soup = BeautifulSoup(content, "lxml")
+        tables = soup.find_all('table')
 
-GenericBank.dump_data(data)
+        quote_date_str = re.findall(ur'Quoted Date[\uff1a][\xa0](.*)\r', tables[4].find_all('td')[:][-1].text)
+        if not quote_date_str:
+            logging.error("Unable to parse quoted date from html!")
+        # Hardcoded as GMT+8 (CST)
+        quote_date = calendar.timegm(time.strptime(quote_date_str[0]+' CST', "%m/%d/%Y %H:%M %Z"))
+
+        data = {}
+        # extract currency exchange data from sixth table
+        for tr in tables[6].find_all('tr'):
+            tds = [td.text for td in tr.find_all('td')]
+            if not tds:
+                continue
+            data[self.currency_table[tds[0]]] = [float(x) if x != '-' else None for x in tds[1:5]]
+        return {"date": quote_date, "data": data}
+
+if __name__ == '__main__':
+    bank = TWBank()
+    data = bank.query_rate()
+    bank.dump_data(data)
+
